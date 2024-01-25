fb03d569da38b55a0b83e3d1960c984e4bbdccea
Raheel534 <145212579+Raheel534@users.noreply.github.com>
2024-01-25 07:29:46 +0000

    DATA types

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert(hello) // we are using node js not browser
+
+console.log("hello")
+
+ console.log("raheel")
+
+ let name ="raheel"
+ let age = 22
+ let isloggedin = false
+
+
+ //string => ""
+ //boolean => true/ false
+ //null => standalone value
+ //undefined => not assigned value 
+ //symbol => to find unique 
+ 
+
+ console.log(typeof null )
