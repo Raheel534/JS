aa422b0753d1b77d981b9ee678310ee5c4ecb533
Raheel534 <145212579+Raheel534@users.noreply.github.com>
2024-01-25 19:45:54 +0000

    conversions

---
+++ 1_basics/3_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = 33
+
+console.log(typeof (score));
+console.log("RAHEEL ")
+let a = 5
+let b = 10
+if(a<b){
+    console.log("a is smaller")
+}
+else{
+    console.log("b is greater")
+}
+alert( 3 + 3);
