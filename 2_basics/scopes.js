a8ab1c4aaf04001e1685a78aeebd883fdda807a8
Raheel534 <145212579+Raheel534@users.noreply.github.com>
2024-01-29 18:47:25 +0000

    scopes

---
+++ 2_basics/scopes.js
@@ -0,0 +1,18 @@
+var c = 30;
+//let b = 20;
+const m =10
+ //console.log(c);
+ //console.log(b);
+ //console.log(a);  
+ 
+ var a = 10
+ {
+    var a = 200 
+    
+ }
+ console.log(a);
+
+ for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+ }
